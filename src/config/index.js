cce5bf1edaa28ec4595a1c5fa019025b14c2ac56
Felipe Estevanatto <estevanatto@estevanatto.localdomain>
2022-08-27 13:45:31 -0300

    env update

--- src/config/index.js
+++ src/config/index.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 module.exports = {
   DB: process.env.MONGO_URI || "mongodb://root:example@mongo:27017/",
   PORT: process.env.APP_PORT || 5000,
-  HOST: '0.0.0.0',
-  SECRET: process.env.APP_SECRET,
+  HOST: process.env.APP_HOST || '0.0.0.0',
+  SECRET: process.env.JWT_SECRET,
   CLIENT_URL: process.env.APP_CLIENT_URL,
   CLIENT_ID: process.env.CLIENT_ID,
 };
