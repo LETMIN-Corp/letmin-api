160457f9ed8631cdb1daee43681cde9eb89199f2
Gabriel Nicolim <gabriel.gomes.nicolim@gmail.com>
2022-10-12 08:13:11 -0300

    Update Complaint.js

--- src/models/Complaint.js
+++ src/models/Complaint.js
@@ -5,7 +5,7 @@ const ComplaintSchema = new Schema({
 	reason: {
 		type: String,
 		required: true,
-		enum: ['Conteúdo Inapropriado', 'Spam', 'Outro']
+		enum: ['Conteúdo inapropriado', 'Spam', 'Outros']
 	},
 	description: {
 		type: String,
